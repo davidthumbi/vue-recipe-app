92b411790fb99aa0eabaddd0f82b2175f0646b6d
David Thumbi <82018958+davidthumbi@users.noreply.github.com>
2024-11-08 09:18:19 +0300

    Update MealDetails.vue

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -22,9 +22,9 @@
       <div>
         <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
         <ul>
-          <template v-for="(el, ind) of new Array(20)">
-            <li v-if="meal[`strIngredient${ind + 1}`]">
-            {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
+          <template v-for="ind in maxIngredients" :key="ind">
+            <li v-if="meal[`strIngredient${ind}`]">
+              {{ ind }}. {{ meal[`strIngredient${ind}`] }}
             </li>
           </template>
         </ul>
@@ -32,9 +32,9 @@
       <div>
         <h2 class="text-2xl font-semibold mb-2">Measures</h2>
         <ul>
-          <template v-for="(el, ind) of new Array(20)">
-            <li v-if="meal[`strMeasure${ind + 1}`]">
-            {{ ind + 1 }}. {{ meal[`strMeasure${ind + 1}`] }}
+          <template v-for="ind in maxIngredients" :key="ind">
+            <li v-if="meal[`strMeasure${ind}`]">
+              {{ ind }}. {{ meal[`strMeasure${ind}`] }}
             </li>
           </template>
         </ul>
@@ -50,25 +50,27 @@
         </a>
       </div>
     </div>
-
-
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { ref, onMounted, computed } from 'vue';
 import { useRoute } from 'vue-router';
 import axiosClient from '../axiosClient';
 import YouTubeButton from '../components/YouTubeButton.vue';
 
 const route = useRoute();
-const meal = ref({})
+const meal = ref({});
 
 onMounted(() => {
   axiosClient.get(`lookup.php?i=${route.params.id}`)
     .then(({ data }) => {
-      meal.value = data.meals[0] || {}
-    })
-})
+      meal.value = data.meals[0] || {};
+    });
+});
 
+// Compute the indexes of available ingredients and measures
+const maxIngredients = computed(() =>
+  Array.from({ length: 20 }, (_, i) => i + 1).filter(i => meal.value[`strIngredient${i}`])
+);
 </script>

5a5b1df8bd4b4cbe182d202fb308230bfefb78fb
David Thumbi <82018958+davidthumbi@users.noreply.github.com>
2024-11-08 09:10:12 +0300

    Update MealItem.vue

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -11,9 +11,9 @@
     </router-link>
     <div class="p-3">
       <h3 class="font-bold">{{ meal.strMeal }}</h3>
-      <p class="mb-4">
-  {{ meal.strInstructions }}
-</p>
+       <p class="mb-4">
+        {{ $filters.truncateWords(meal.strInstructions, 20) }}
+      </p>
       <div class="flex items-center justify-between">
         <YouTubeButton :href="meal.strYoutube" />
       </div>
